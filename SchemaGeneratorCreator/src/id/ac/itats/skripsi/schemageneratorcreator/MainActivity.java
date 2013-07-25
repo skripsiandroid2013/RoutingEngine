ce11a8967cddcaf86e8eab85e5d2cb04d5440b87
Abdillah Aziz <mr_pupu@yahoo.com>
2013-07-25 15:54:23 +0700

    fixed buildpath

---
+++ SchemaGeneratorCreator/src/id/ac/itats/skripsi/schemageneratorcreator/MainActivity.java
@@ -0,0 +1,37 @@
+package id.ac.itats.skripsi.schemageneratorcreator;
+
+import id.ac.itats.skripsi.orm.DaoMaster;
+import id.ac.itats.skripsi.orm.DaoMaster.DevOpenHelper;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	private SQLiteDatabase db;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "GraphDB-4",
+				null);
+		db = helper.getWritableDatabase();
+		new DaoMaster(db);
+	
+		
+		Log.i("Schema Creator", "Success!");
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
